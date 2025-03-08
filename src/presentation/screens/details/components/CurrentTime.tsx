bebfcd5d46126141e0af5fa3f963b7c781716fbb
Cristian Milan <danilomillan108@gmail.com>
2025-03-08 18:16:26 -0500

    mostrar la fecha

--- src/presentation/screens/details/components/CurrentTime.tsx
+++ src/presentation/screens/details/components/CurrentTime.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {colors} from '../../../../constants/colors';
 import {Weather} from '../../../../data/models/Weather';
 
@@ -9,15 +9,25 @@ interface CurrentTimeProps {
 
 const CurrentTime: React.FC<CurrentTimeProps> = ({weather}) => {
   const [currentTime, setCurrentTime] = useState<string>('');
+  const [currentDate, setCurrentDate] = useState<string>('');
 
   useEffect(() => {
     if (weather) {
       const interval = setInterval(() => {
         const date = new Date((Date.now() / 1000 + weather.timezone) * 1000);
         setCurrentTime(
-          date.toLocaleTimeString('en-US', {
+          date.toLocaleTimeString('es-CO', {
             hour: '2-digit',
             minute: '2-digit',
+            timeZone: 'UTC',
+          }),
+        );
+        setCurrentDate(
+          date.toLocaleDateString('es-CO', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
             timeZone: 'UTC',
           }),
         );
@@ -26,12 +36,25 @@ const CurrentTime: React.FC<CurrentTimeProps> = ({weather}) => {
     }
   }, [weather]);
 
-  return <Text style={styles.currentTime}>{currentTime}</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.currentTime}>{currentTime}</Text>
+      <Text style={styles.currentDate}>{currentDate}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
   currentTime: {
     fontSize: 18,
+    color: colors.white,
+    textAlign: 'center',
+  },
+  currentDate: {
+    fontSize: 16,
     color: colors.white,
     textAlign: 'center',
   },
