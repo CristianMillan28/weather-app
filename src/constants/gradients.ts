b53e5ae49106fc6a7835d8075274992c958dd8d1
Cristian Milan <danilomillan108@gmail.com>
2025-03-08 18:13:48 -0500

    listo los gradientes

--- src/constants/gradients.ts
+++ src/constants/gradients.ts
@@ -1,6 +1,6 @@
 export const gradients = {
   night: ['#02091C', '#001950', '#00296A', '#03408A', '#1075AB'],
   dawn: ['#7D74A1', '#A08BC4', '#D8A2C4', '#FEBFC7', '#FED2BC', '#FBEBCB'],
-  day: ['#ADFAFF', '#80A8FF'],
+  day: ['#0075ef', '#0093f1', '#28a9f3', '#4bb8f4', '#75c9f5'],
   dusk: ['#432967', '#782D65', '#C3305D', '#F0445E', '#FD9367', '#FDCD83'],
 };
